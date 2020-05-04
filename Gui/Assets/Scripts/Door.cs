458b056910960c0799710faf4f0273a43b01ed37
s19600 <61684591+s19600@users.noreply.github.com>
2020-05-04 23:36:32 +0200

    v.0.1

---
+++ Gui/Assets/Scripts/Door.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Door : MonoBehaviour
+{
+    public int levelToLoad;
+    private SceneSwitcher sceneSwitcher;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneSwitcher = GameObject.Find("SceneSwitcher").GetComponent<SceneSwitcher>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Collision.gameObject.CompareTag("Player"))
+        {
+            sceneSwitcher.LoadLevel(levelToLoad);
+        }
+    }
+}
---
+++ Gui/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class GameManager : MonoBehaviour
+{
+    
+    public Text gameOverText;
+    private bool gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            if(Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                PlayerPrefs.SetInt(ScorePrefs.SCORE, 0);
+                PlayerPrefs.SetInt(ScorePrefs.PREVIOUS_SCORE, 0);
+                SceneManager.LoadScene("Menu");
+            }
+        }
+    }
+
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over";
+        gameOver = true;
+        PlayerPrefs.SetInt(ScorePrefs.SCORE, PlayerPrefs.GetInt(ScorePrefs.PREVIOUS_SCORE));
+    }
+
+
+}
+
+
